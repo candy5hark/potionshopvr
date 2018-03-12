e28b588e0f578d7f367218ae888d0373db1b20aa
Autumn Raines <xomies@gmail.com>
2018-03-12 00:14:41 -0500

    Start ingredient descriptions

---
+++ Potion Shop/Assets/Scripts/Text_Activator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text_Activator : MonoBehaviour {
+
+	public GameObject ingredient_info;
+	public GameObject line_trigger;
+
+	
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject == line_trigger)
+		{
+			ingredient_info.SetActive(true);
+		}
+	}
+	void OnTriggerExit(Collider col){
+		if(col.gameObject == line_trigger)
+		{
+			ingredient_info.SetActive(false);
+		}
+	}
+
+}
